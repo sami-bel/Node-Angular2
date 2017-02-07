81357fed9a7f4d499aa2277697324a469a43fbbb
sami <samsoum.infor@gmail.com>
2017-02-07 23:07:04 +0100

    first comment

---
+++ public/my-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+//
+@Component({
+  selector: 'my-app',
+  templateUrl: '/my-app/src/app/app.component.html',
+  styleUrls: ['my-app/src/app/app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
